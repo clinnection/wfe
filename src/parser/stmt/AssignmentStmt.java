899a7a27f463c49ffc3674198e9ac1bdea030c16
John W. Carbone <jwc.clinnection@gmail.com>
2022-04-02 10:26:36 -0400

    Make Assignment class abstract and add toJsonObject

--- src/parser/stmt/AssignmentStmt.java
+++ src/parser/stmt/AssignmentStmt.java
@@ -1,9 +1,10 @@
 package parser.stmt;
 
+import org.json.JSONObject;
 import parser.expr.Expr;
 import parser.var.Var;
 
-public class AssignmentStmt extends Stmt {
+public abstract class AssignmentStmt extends Stmt {
     private Expr expr;
     private Var var;
 
@@ -27,5 +28,14 @@ public class AssignmentStmt extends Stmt {
 
     public void setVar(Var var) {
         this.var = var;
+    }
+
+    @Override
+    public JSONObject toJsonObject() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.putOnce("type", type);
+        jsonObject.putOnce("var", var.toJsonObject());
+        jsonObject.putOnce("expr", expr.toJsonObject()); // TODO: implement
+        return jsonObject;
     }
 }
